431e6bced4bc33bca7da6d3a2725d183d98fe6c3
tigr124652 <141646930+tigr124652@users.noreply.github.com>
2024-01-07 19:46:04 +0300

    Delete bot/mydata.py

--- bot/mydata.py
+++
@@ -1,7 +0,0 @@
-TOKEN = "6971763715:AAGL3ItMFKle6UOzFT1IawVaWg3u-eatOl4"
-BOT = "https://api.telegram.org/bot"
-TEXT = "Чтобы ты не говорил ты все равно пупсик"
-
-chat_id: int
-offset = -2
-timeout = 60
